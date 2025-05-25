66af4d53ec33f8825c84756001920bd5b350487e
Jetlagg <jetja123456@gmail.com>
2025-05-25 17:54:35 +0700

    add color.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
 function Footer() {
   return (
-    <footer className="bg-white border-t mt-10 py-6 text-center text-sm text-gray-600">
-      <p>&copy; 2025 CacheCash Corporation</p>
-      <div className="mt-2 flex justify-center gap-4">
-        <a href="#">ช่วยเหลือ</a>
-        <a href="#">ข้อกำหนดการใช้</a>
-        <a href="#">นโยบายความเป็นส่วนตัว</a>
+    <footer className="bg-white mt-10 py-6 text-center text-sm text-gray-500">
+      <div className="flex justify-between">
+        <div className="flex justify-center gap-4">
+          <p>&copy; 2025 CacheCash Corporation</p>
+          <a href="#">ช่วยเหลือ</a>
+          <a href="#">ข้อกำหนดการใช้</a>
+          <a href="#">นโยบายความเป็นส่วนตัว</a>
+        </div>
+      
+        <div className="flex justify-center">
+          <p>dkfjkdfjlsdfj</p>
+        </div>
       </div>
     </footer>
   )
